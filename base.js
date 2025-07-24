1d23cfecf217e7b8279cbab9c568b82ca8e6cd46
Gaurav <gauravshukla5882@gmail.com>
2025-07-24 11:50:09 +0530

    here is my string and array practice

---
+++ base.js
@@ -0,0 +1,73 @@
+// let is ="123"
+// let newID = Number(id);
+// console.log(typeof newID);
+// let user="Gaurav";
+// let age=17;
+// let email="gaurav@gmail.com";
+// console.log("user is",user,"and age is",age,"email is",email);
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+// let x=3;
+// let y=x++;
+// console.log(x);
+// console.log(y);
+// console.log(3 == "3");
+// console.log(3 === "3");
+// let x=10;
+// console.log(x++ + ++x);
+// let result = 5+3*2/(4-2);
+// console.log(result);
+// console.log(0 == false);
+// console.log(0 === false);
+// console.log(null == undefined);
+// console.log(null === undefined);
+
+
+// let marks = 100;
+// if(marks>=90){
+//     console.log("Grade A")
+// } 
+// else if(marks>=75){
+//    console.log("Grade B")
+// }
+// else{
+//     console.log("Grade C")
+// }
+
+
+// let age =69;
+// let result =(age>=18)? "ADULT": "Minor";
+// console.log(result);
+
+let day = 4
+
+switch (day) {
+    
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("Thursday");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+    case 6:
+        console.log("Saturday");
+        break;
+    case 7:
+        console.log("Sunday");
+        break;
+    default:
+        console.log("Invalid day");
+} 
+
+for (let i=0; i<5; i++){
+    console.log("hello world");
+}
---
+++ timepass.js
@@ -0,0 +1,105 @@
+// let num=null?? 10;
+// console.log(num);
+
+// let num=undefined ?? 10;
+// console.log(num);
+
+
+// function printgreeting(name){
+//     return`Hello how are you ${name}`;
+// }
+// console.log(printgreeting("bot"));
+// (printgreeting("noob"));
+// (printgreeting("boo"));
+// (printgreeting("boyttt"));
+
+
+// function addNum(num1,num2){
+//     let n1=Number(num1);
+//     let n2=Number(num2);
+
+//     return n1+n2;
+// }
+// console.log(addNum(10,"20"));
+
+
+// function addNum(num1,num2){
+//     let n1=Number(num1);
+//     let n2=Number(num2);
+
+//     if(isNaN(n1) || isNaN(n2)){
+//         return ("Enter proper number");
+//     }
+//     return n1+n2;
+// }
+// console.log(addNum(10,"20abc"));
+
+// let string1="gaurav";
+// let string2=new String("gaurav shukla")
+// console.log(typeof string1);
+// console.log(typeof string2);
+
+                            // ----------------//
+// let user = "BOOMRAH";
+// console.log(user.length)
+
+// console.log(user.toUpperCase());
+// console.log(user.toLowerCase());
+
+// console.log(user.charAt(5));
+// console.log(user.indexOf('BOOM'));
+
+// // user.replace(oldVs1.newVal1);
+// console.log(user.replace("M","O"));
+// console.log(user);
+
+// let userName = "sai%deep%noob%gaurav"
+// console.log(userName.split('%'));
+
+// let user_2 = "saideep"
+// console.log(user_2.substring("4,6"));
+// console.log(user_2.slice(-2,3));
+// console.log(user_2.substring(-2,3));
+// console.log(user_2.slice(4,6));
+
+// console.log(user.substring(4,2));
+// console.log(user.slice(4,2));
+
+// substring vs slice
+//negative index behaviour (possible in slice)
+//swaping index (possible in substring)
+// let user = "ganesh";
+// for (const element of user)
+// {
+//     console.log(element);
+// }
+
+//for.. of loop
+//it givees value
+//it is applicable for string,array,map
+//not applicable for object
+// let user1="saideep"
+// console.log(user1=[10,20,30,40]);
+// console.log(Array.isArray(user1));
+
+//araayOf
+// let num=10;
+// let num1=20;
+// let num2=30;
+// let num3=40;
+// console.log(Array.of(num,num1,num2,num3));
+
+
+// const user=[10,20,30,40,50];
+// console.log("before",user);
+// const newuser=user.splice(0,3);
+// console.log("newuser",newuser);
+// console.log("after",user);
+
+//for ..Of array
+const user=[10,20,30,40,50];
+for(const value of user){
+    console.log(value);
+}
+
+const userId=[10,20,30,40,50];
